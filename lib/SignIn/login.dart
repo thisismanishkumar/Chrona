c76bfe8339f35503e1d8f50a0ee74f130688f288
Aritra Chatterjee <aritrachatterjee07@gmail.com>
2020-07-31 22:52:48 +0530

    Added splash screen

--- lib/SignIn/login.dart
+++ lib/SignIn/login.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:chrona_1/Activities/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'state_widget.dart';
 import 'package:chrona_1/UserInfo/state.dart';
 class LoginScreen extends StatefulWidget {
@@ -9,44 +12,81 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 4), () => gotoHome(context));
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text('Google Login'),
-
-      ),
-      body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RaisedButton(
-                  onPressed: () { if(StaticState.user==null)StateWidget.of(context).signInWithGoogle().then((val)=>Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomeScreen())));},
-                  padding: EdgeInsets.only(top: 3.0, bottom: 3.0, left: 3.0),
-                  color: const Color(0xFFFFFFFF),
-                  child: new Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      new Image.asset(
-                        'images/g1.jpg',
-                        height: 40.0,
-                      ),
-                      new Container(
-                          padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                          child: new Text(
-                            "Sign in with Google",
-                            style: TextStyle(
-                                color: Colors.grey,
-                                fontWeight: FontWeight.bold),
-                          )),
-                    ],
-                  )
-              ),
-            ],
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.redAccent),
           ),
-        ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 50.0,
+                        child: Icon(
+                          Icons.developer_mode,
+                          color: Colors.greenAccent,
+                          size: 50.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                        "Chrona",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24.0),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                    ),
+                    Text(
+                      "Get the information you need. Anytime. Anywhere.",
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                          color: Colors.white),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
+  }
+  void gotoHome(BuildContext context){
+    if(StaticState.user==null)StateWidget.of(context).signInWithGoogle()
+    .then((val)=>Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomeScreen())))
+    .catchError((err)=>SystemNavigator.pop());
   }
 }
